e64eac8a1416fac87fb1136c2981b42ae6c748d1
Utkarshh014 <shekharutkarsh75@gmail.com>
2025-06-04 17:49:49 +0530

    made header

---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Header.css';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import SearchIcon from '@mui/icons-material/Search';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className='header__left'>
+        <AccountCircleIcon className='header_avatar' alt='username' src=''/>
+        <AccessTimeIcon/>
+        </div>
+        <div className='header__search'>
+        <SearchIcon/>
+        <input type="text" placeholder='Search Teamflow'/>
+
+        </div>
+        <div className='header__right'>
+        <HelpOutlineIcon/>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
