46ac5932d4c9f955c5ccb0c880c8d14e0cc502a3
Utkarshh014 <shekharutkarsh75@gmail.com>
2025-06-04 20:45:58 +0530

    created header and sidebar header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import './Header.js';
 import Header from './Header.js';
+import Sidebar from './Sidebar.js';
 
 function App() {
   return (
     <div className="App">
-    <Header></Header>
+    <Header/>
+
+    <div className='app__body'>
+      <Sidebar/>
+      {/* Sidebar */}
+      {/* React-Router -> Chat Screen */}
+    </div>
+
     </div>
   );
 }
